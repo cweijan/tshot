3939d76bb1bf6b3555318fc507836c883895e4b4
cweijan <cweijan@163.com>
2024-10-07 12:04:18 +0800

    移除jest

--- example/example.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-import { b } from './example-b'
-import { watch } from 'chokidar'
-
-console.log(path.join(__dirname, b, 'xx'), watch)
--- src/test/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import { run } from '..'
-import { join } from 'path'
-
-// ts-node --skip-project src\test\index.spec.ts
-run(join(__dirname,'./test.ts'),true)
